bd8d8e836fb51cf3965bff92206b6afe54dde689
djhofreiter <djhofreiter@gmail.com>
2017-06-29 11:53:22 -0400

    Shapes javascript

--- basic-js/obj1/obj1.js
+++ basic-js/obj1/obj1.js
@@ -5,13 +5,24 @@ window.onload = function() {
         this.height = height;
         this.width = width;
     }
-
+    
+    Shape.prototype.Area = function() {
+        return this.height * this.width;
+            }
     var square = new Shape("square", 10, 10);
-    var triangle = new Shape("triangle", 3, 4);
     var rectangle = new Shape("rectangle", 4, 6);
+    var triangle = new Shape("triangle", 3, 4);
 
-console.log(Shape);
 console.log(square);
+console.log(square.Area());
 console.log(triangle);
+console.log(triangle.Area());
 console.log(rectangle);
+console.log(rectangle.Area());
+
+    String.prototype.prefix = function(msg) {
+        return msg +  " " + this;
+    }
+
+    console.log("world".prefix("hello"));
 };
