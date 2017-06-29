850af22bedd5039695a526fb1ccb06abeae54596
djhofreiter <djhofreiter@gmail.com>
2017-06-29 14:53:17 -0400

    Promise sample

--- basic-js/promise/promise.js
+++ basic-js/promise/promise.js
@@ -1,2 +1,16 @@
 'use strict';
 var c = console;
+
+function getPromise(msg) {
+    var val = new Promise(function(res, rej) {
+        res(msg + " promise resolved");
+        rej(msg + " promise has been rejected");
+    })
+    return val;
+}
+
+var thePromise = getPromise("hello");
+
+thePromise.then(function(data) {
+    c.log(data);
+})
