e74f5c9e5f142d8f68c509bae923524c44e94edf
djhofreiter <djhofreiter@gmail.com>
2017-06-29 20:26:28 -0400

    update todo

--- basic-js/todolist/todo1.js
+++ basic-js/todolist/todo1.js
@@ -1,16 +1,34 @@
 
 var addElm = document.getElementById("add")
-// elem.appendChild(obj)
+var element = document.getElementById("mylist");
+var object = document.createElement("p");
+
+// Creates a new task after clicking the add button
 addElm.addEventListener("click", function(){
 newTask = document.getElementById("todo");
 task = newTask.value;
-
-var element = document.getElementById("mylist");
-var object = document.createElement("p");
-element.appendChild(object);
 var toDo = document.createTextNode(task);
-object.appendChild(toDo);
+element.appendChild(object);
 
+object.appendChild(toDo);
+deleteButton();
 
 })
-var del = createElement("delete")
+
+// Creates a button for deleting
+var del = document.createElement("button")
+
+function remove () {
+    element.removeChild(object);
+    
+}
+
+
+// Inserts delete button next to task
+function deleteButton () {
+element.appendChild(object);
+object.appendChild(del);
+del.innerHTML="Delete";
+del.onclick = remove();
+
+}
