10ead0595de48913ae9c385404149d83845684df
djhofreiter <djhofreiter@gmail.com>
2017-06-30 16:19:50 -0400

    modulation sample

--- basic-js/module/main.js
+++ basic-js/module/main.js
@@ -1,6 +1,11 @@
 // Anonymous function
-(function(module2){
+window.app = (function(module2){
     c.log("hello")
     c.log(module2.publicVar);
-
+    window.onload = function() {
+    module2.doPost(function(data) {
+        c.log(data);
+        document.getElementById("main").innerText = data;
+    });
+    }
  })(window.app2 || (window.app2) ); //IIFE function
--- basic-js/module/svcapp.js
+++ basic-js/module/svcapp.js
@@ -1,8 +1,28 @@
 window.app2 = (function(){
 
 var privateVar = "I'm a private variable";
+function doPost(callback) {
+    var url = "http://jsonplaceholder.typicode.com/posts";
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+            if (xhr.readyState ==4 && xhr.status ==201) {
+                var data = xhr.responseText;
+                callback(data)
+            }
+    }
+    var dataToSend = {
+        title: "Fred",
+        body: "Trouble Maker",
+        userID: 1
+    };
+    xhr.open("POST", url, true);
+    xhr.send(dataToSend);
+
+}
 
 return {
-    publicVar: privateVar
+    publicVar: privateVar,
+    doPost: doPost,
 }
  })(); //IIFE function
